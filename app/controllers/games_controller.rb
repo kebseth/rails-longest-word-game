f97556aaafa2da2e7857af6fb5175f612ea89633
Richard Bouvet <bouvet.richard@hotmail.fr>
2019-08-05 17:19:11 +0200

    youpi

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,8 +1,40 @@
+require 'open-uri'
+require 'json'
+
 class GamesController < ApplicationController
   def new
     @letters = ('A'..'Z').to_a.sample(10)
   end
 
   def score
+    # raise
+    @letters = params[:token].split('')
+    @answer = params[:answer].upcase
+
+    if !in_the_grid?
+      @result = :not_in_the_grid
+    elsif english_word?
+      @result = :not_an_english_word
+    else
+      @result = :congrats
+    end
+  end
+
+  private
+
+  def in_the_grid?
+    @array_answer = @answer.split('')
+    @suppr_array = @letters
+    @array_answer.all? do |letter|
+      @array_answer.count(letter) <= @letters.count(letter)
+    end
+  end
+
+  def english_word?
+    url = "https://wagon-dictionary.herokuapp.com/#{params[:answer].upcase}"
+    @serialized_word = open(url).read
+    @parse_word = JSON.parse(@serialized_word)
+    return @parse_word['found'] == true
   end
 end
+
